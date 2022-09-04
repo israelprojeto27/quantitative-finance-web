f084228fd7a222ea9038bced033b8f8e44ecf713
Israel Barreto <israelprojeto27@gmail.com>
2022-09-04 14:03:46 -0300

    Adição das funcionalidades de Analises em todos os modulos

--- constants/constants.js
+++ constants/constants.js
@@ -9,6 +9,8 @@ export const ACAO_DIVIDENDO_URL = BASE_URL + '/dividendo-acao';
 
 export const ACAO_SIMULACAO_INVESTIMENTO_URL = BASE_URL + '/simula-investimento-acao';
 
+export const ACAO_ANALISE_URL = BASE_URL + '/analise-acao';
+
 export const FUNDO_IMOBILIARIO_URL = BASE_URL + '/fundo-imobiliario';
 
 export const FUNDO_IMOBILIARIO_DIVIDENDO_URL = BASE_URL + '/dividendo-fundo';
@@ -16,6 +18,8 @@ export const FUNDO_IMOBILIARIO_DIVIDENDO_URL = BASE_URL + '/dividendo-fundo';
 export const FUNDO_IMOBILIARIO_COTACAO_URL = BASE_URL + '/cotacao-fundo';
 
 export const FUNDO_IMOBILIARIO_SIMULACAO_INVESTIMENTO_URL = BASE_URL + '/simula-investimento-fundo-imobiliario';
+
+export const FUNDO_IMOBILIARIO_ANALISE_URL = BASE_URL + '/analise-fundo';
 
 export const EXTRATO_URL = BASE_URL + '/extrato';
 
@@ -26,6 +30,8 @@ export const BDR_DIVIDENDO_URL = BASE_URL + '/dividendo-bdr';
 export const BDR_COTACAO_URL = BASE_URL + '/cotacao-bdr';
 
 export const BDR_SIMULACAO_INVESTIMENTO_URL = BASE_URL + '/simula-investimento-bdr';
+
+export const BDR_ANALISE_URL = BASE_URL + '/analise-bdr';
 
 export const PARAMETRO_URL = BASE_URL + '/parametro';
 
